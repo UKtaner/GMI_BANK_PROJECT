bc1ef465b7693ed0925d8efb47e8c816136d1f57
UKtaner <tanercakin01@gmail.com>
2021-11-21 14:53:13 +0000

    runner commit

--- src/test/java/gmibank/runners/Runner.java
+++ src/test/java/gmibank/runners/Runner.java
@@ -1,4 +1,24 @@
 package gmibank.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/default-cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml",
+                "rerun:target/failedRerun.txt",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        },
+        features = "./src/test/resources/features",
+        glue = "step_definitions",
+        tags = "@",
+        dryRun = false
+)
 public class Runner {
+
 }
