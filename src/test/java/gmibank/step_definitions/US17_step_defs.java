7c76f3d74b95f6c2650dcb1a314885c7e8bd5055
OCodeCyber <demirorhan0555@gmail.com>
2021-11-22 17:26:07 +0100

    login page

---
+++ src/test/java/gmibank/step_definitions/US17_step_defs.java
@@ -0,0 +1,18 @@
+package gmibank.step_definitions;
+
+import gmibank.utilities.ConfigReader;
+import gmibank.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+
+public class US17_step_defs {
+    @Given("admin on home page")
+    public void admin_on_home_page(){
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+    }
+
+    @And("admin go login page")
+    public void admin_go_login_page(){
+
+    }
+}
