7fb9faac176dd96aebde996c9f9d201889765b1d
Tugba <seriktuba084@gmail.com>
2021-11-23 11:42:24 -0500

    Tugba

--- src/test/java/gmibank/pages/LoginPage.java
+++ src/test/java/gmibank/pages/LoginPage.java
@@ -1,4 +1,15 @@
 package gmibank.pages;
 
+import gmibank.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+
+
+
+
 }
+
+
