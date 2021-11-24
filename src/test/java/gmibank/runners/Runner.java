3e9972380fc1a3d923a5affcf6fadbd186a8b885
SatiCelik <ruzgarssgf@gmail.com>
2021-11-24 23:53:40 +0100

    US_002,Invalid part

--- src/test/java/gmibank/runners/Runner.java
+++ src/test/java/gmibank/runners/Runner.java
@@ -15,8 +15,8 @@ import org.junit.runner.RunWith;
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
         },
         features = "src/test/resources/features",
-        glue = "step_definitions",
-        tags = "@registration_info",
+        glue = "gmibank/step_definitions",
+        tags = "@ssn_test_invalid",
 
         dryRun = false
 
