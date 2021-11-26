035d24cb5d84f2073165c2d2a709e7accadd398e
Taner <taner.turkili@gmail.com>
2021-11-25 20:38:47 -0500

    Final version

--- src/test/java/gmibank/runners/Runner.java
+++ src/test/java/gmibank/runners/Runner.java
@@ -16,8 +16,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "gmibank/step_definitions",
-        tags = "@create_a_new_customer",
-
+        tags = "@customers_info",
         dryRun = false
 )
 
