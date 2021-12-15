ed320812012d7a35765498af04dd7904c608dbf5
UKtaner <tanercakin01@gmail.com>
2021-12-15 13:48:52 +0000

    conflict

---
+++ src/test/java/gmibank/pojos/StatePojo.java
@@ -0,0 +1,60 @@
+package gmibank.pojos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class StatePojo {
+
+    private int id;
+    private String name;
+    private String tpcountry;
+
+    /*{
+    "id": 103694,
+    "name": "xxxxxxxVirginiaxxxxx",
+    "tpcountry": null
+}*/
+
+    public StatePojo() {
+    }
+
+    public StatePojo(int id, String name, String tpcountry) {
+        this.id = id;
+        this.name = name;
+        this.tpcountry = tpcountry;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTpcountry() {
+        return tpcountry;
+    }
+
+    public void setTpcountry(String tpcountry) {
+        this.tpcountry = tpcountry;
+    }
+
+    @Override
+    public String toString() {
+        return "StatePojo{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", tpcountry='" + tpcountry + '\'' +
+                '}';
+    }
+}
